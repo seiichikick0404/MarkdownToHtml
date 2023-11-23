05d96b7b4eb032d860f16727636117d127dbe575
seiichi0404 <yamayamaseisei1834@gmail.com>
2023-11-23 16:51:15 +0900

    stateで条件分岐

--- parser.php
+++ parser.php
@@ -5,7 +5,7 @@ $parsedown = new Parsedown();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $markdown = $_POST['markdown'];
-   
+
     if ($_POST['state'] === 'preview') {
         echo $parsedown->text($markdown);
     } elseif ($_POST['state'] === 'html') {
